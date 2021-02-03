232712043b770dae815c2da5433d9ec3d186bf3a
mori30r <ghahreman00@gmail.com>
2021-02-03 20:06:07 +0330

    dummy data and noice models added

---
+++ models/Noice.js
@@ -0,0 +1,9 @@
+export class Noice {
+  constructor(voice, note, title, isDone, isFavorite) {
+    this.voice = voice;
+    this.title = title;
+    this.note = note;
+    this.isDone = isDone;
+    this.isFavorite = isFavorite;
+  }
+}
