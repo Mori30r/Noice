2f8b30ebf7b25b94cd1496e6692610dd0de617a8
mori30r <ghahreman00@gmail.com>
2021-02-03 19:38:25 +0330

    header style completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,98 +1,103 @@
-import React from 'react';
-import {Text, View, StyleSheet, Dimensions, Button} from 'react-native';
+import React from "react";
+import { EvilIcons } from "@expo/vector-icons";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 
 const HomeScreen = () => {
-    return (
-        <View style={styles.container} >
-            <View style={styles.circle}/>
-            <View style={styles.header}>
-                <View style={styles.iconsContainer} >
-                    <Button color="#354895" title="Settings" style={styles.iconHeader} onPress={()=>{}}/>
-                    <Button color="#354895" title="Search" style={styles.iconHeader} onPress={()=>{}}/>
-                </View>
-                <View style={styles.nav}>
-                    <View style={styles.headerTextContainer}>
-                        <Text style={styles.headerText}>Your {'\n'}Noices</Text>
-                        <View style={styles.progressBarContainer}>
-                            <View style={styles.progressBarEmpty}/>
-                            <View style={styles.progressBarFull}/>
-                        </View>
-                    </View>
-                    <View>
-
-                    </View>
-                </View>
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.circle} />
+      <View style={styles.header}>
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity>
+            <EvilIcons name="gear" size={30} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <EvilIcons name="search" size={30} color="white" />
+          </TouchableOpacity>
         </View>
-    );
+        <View style={styles.nav}>
+          <Text style={styles.headerText}>All Noices 😁</Text>
+          <Text style={styles.smallText}>9/20 Of Noices Are Done !</Text>
+          <View style={styles.progressBarContainer}>
+            <View style={styles.progressBarFull} />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
-const {height, width} = Dimensions.get('screen')
-
+const { height, width } = Dimensions.get("screen");
 
 const styles = StyleSheet.create({
-    container:{
-        height: height,
-        width: width,
-        backgroundColor: '#001341'
-    },
-    header:{
-        backgroundColor: "#a68eff",
-        height: height/3.5,
-        borderBottomLeftRadius: width/10,
-        borderBottomRightRadius: width/10,
-        justifyContent: 'space-around'
-    },
-    iconsContainer:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-end',
-        paddingHorizontal: '5%',
-        minHeight: '8%'
-    },
-    iconHeader:{},
-    circle: {
-        width: width,
-        height: width,
-        borderRadius: width/2,
-        backgroundColor: "#ffffff",
-        position: "absolute",
-        top: height/1.5,
-        left: width/2.3,
-        opacity: .2
-    },
-    nav:{
-        width: '100%',
-        flex: 1,
-        paddingHorizontal: width/20,
-    },
-    headerTextContainer:{
-        paddingTop: 30,
-        paddingLeft: 5,
-        // backgroundColor: 'black',
-    },
-    headerText:{
-        fontSize: 25,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    progressBarContainer:{
-
-    },
-    progressBarEmpty:{
-        position: 'absolute',
-        width: width/4,
-        height: 5,
-        backgroundColor: 'white',
-        borderRadius: 10
-    },
-    progressBarFull:{
-        position: 'absolute',
-        width: width/5,
-        height: 5,
-        backgroundColor: 'green',
-        borderRadius: 10
-    },
-})
+  container: {
+    height: height,
+    width: width,
+    backgroundColor: "#001341",
+  },
+  header: {
+    backgroundColor: "#a68eff",
+    height: height / 3.5,
+    borderBottomLeftRadius: width / 10,
+    borderBottomRightRadius: width / 10,
+    justifyContent: "space-around",
+  },
+  iconsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    paddingHorizontal: "5%",
+    minHeight: "8%",
+  },
+  iconHeader: {},
+  circle: {
+    width: width,
+    height: width,
+    borderRadius: width / 2,
+    backgroundColor: "#ffffff",
+    position: "absolute",
+    top: height / 1.5,
+    left: width / 2.3,
+    opacity: 0.2,
+  },
+  nav: {
+    flex: 1,
+    paddingHorizontal: width / 20,
+    paddingTop: 30,
+    paddingLeft: 5,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "white",
+  },
+  progressBarContainer: {
+    flexDirection: "row",
+    borderRadius: 10,
+    borderWidth: 0.2,
+    marginBottom: 20,
+    width: width / 2,
+    backgroundColor: "#ffffff",
+  },
+  progressBarFull: {
+    width: "50%",
+    height: 8,
+    backgroundColor: "green",
+    borderRadius: 10,
+  },
+  smallText: {
+    fontSize: 15,
+    fontWeight: "200",
+    color: "white",
+  },
+});
 
 export default HomeScreen;
